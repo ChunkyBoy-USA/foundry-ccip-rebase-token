4615e8d9ba00639e323385744db05a619ae0d89b
JimmyMa <ma417704684@gmail.com>
2026-03-28 14:49:36 +0800

    Remove sensitive info

--- bridgeToZKsync.sh
+++ bridgeToZKsync.sh
@@ -3,12 +3,12 @@
 # Define constants 
 AMOUNT=2000000
 
-ZKSYNC_REGISTRY_MODULE_OWNER_CUSTOM="0xaD417c0611dBD225471D31F056b8B6beC1CBC153"
-ZKSYNC_TOKEN_ADMIN_REGISTRY="0x8126bE56454B628a88C17849B9ED99dd5a11Bd2f"
-ZKSYNC_ROUTER="0x2a9C5afB0d0e4BAb2BCdaE109EC4b0c4Be15a165"
-ZKSYNC_RNM_PROXY_ADDRESS="0x9527E2d01A3064ef6b50c1Da1C0cC523803BCFF2"
-ZKSYNC_SEPOLIA_CHAIN_SELECTOR="3478487238524512106"
-ZKSYNC_LINK_ADDRESS="0xb1D4538B4571d411F07960EF2838Ce337FE1E80E"
+ARBITRUM_REGISTRY_MODULE_OWNER_CUSTOM="0xaD417c0611dBD225471D31F056b8B6beC1CBC153"
+ARBITRUM_TOKEN_ADMIN_REGISTRY="0x8126bE56454B628a88C17849B9ED99dd5a11Bd2f"
+ARBITRUM_ROUTER="0x2a9C5afB0d0e4BAb2BCdaE109EC4b0c4Be15a165"
+ARBITRUM_RNM_PROXY_ADDRESS="0x9527E2d01A3064ef6b50c1Da1C0cC523803BCFF2"
+ARBITRUM_SEPOLIA_CHAIN_SELECTOR="3478487238524512106"
+ARBITRUM_LINK_ADDRESS="0xb1D4538B4571d411F07960EF2838Ce337FE1E80E"
 
 SEPOLIA_REGISTRY_MODULE_OWNER_CUSTOM="0xa3c796d480638d7476792230da1E2ADa86e031b0"
 SEPOLIA_TOKEN_ADMIN_REGISTRY="0x95F29FEE11c5C55d26cCcf1DB6772DE953B37B82"
@@ -20,28 +20,28 @@ SEPOLIA_LINK_ADDRESS="0x779877A7B0D9E8603169DdbD7836e478b4624789"
 # Compile and deploy the Rebase Token contract
 source .env
 forge build
-echo "Compiling and deploying the Rebase Token contract on ZKsync..."
-ZKSYNC_REBASE_TOKEN_ADDRESS=$(forge create src/RebaseToken.sol:RebaseToken --legacy  --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  | awk '/Deployed to:/ {print $3}')
-echo "ZKsync rebase token address: $ZKSYNC_REBASE_TOKEN_ADDRESS"
-# ZKSYNC_REBASE_TOKEN_ADDRESS="0x0b24eD66b4bd34d6C9BF070F108B8E4b2786A06b"
+echo "Compiling and deploying the Rebase Token contract on ARBITRUM..."
+ARBITRUM_REBASE_TOKEN_ADDRESS=$(forge create src/RebaseToken.sol:RebaseToken --legacy  --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  | awk '/Deployed to:/ {print $3}')
+echo "ARBITRUM rebase token address: $ARBITRUM_REBASE_TOKEN_ADDRESS"
+# ARBITRUM_REBASE_TOKEN_ADDRESS="0x0b24eD66b4bd34d6C9BF070F108B8E4b2786A06b"
 # # Compile and deploy the pool contract
-echo "Compiling and deploying the pool contract on ZKsync..."
-pool_output=$(forge create src/RebaseTokenPool.sol:RebaseTokenPool --legacy --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --constructor-args ${ZKSYNC_REBASE_TOKEN_ADDRESS} '[]' ${ZKSYNC_RNM_PROXY_ADDRESS} ${ZKSYNC_ROUTER})
+echo "Compiling and deploying the pool contract on ARBITRUM..."
+pool_output=$(forge create src/RebaseTokenPool.sol:RebaseTokenPool --legacy --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --constructor-args ${ARBITRUM_REBASE_TOKEN_ADDRESS} '[]' ${ARBITRUM_RNM_PROXY_ADDRESS} ${ARBITRUM_ROUTER})
 echo "$pool_output"
-ZKSYNC_POOL_ADDRESS=$(echo "$pool_output" | awk '/Deployed to:/ {print $3}')
-# ZKSYNC_POOL_ADDRESS="0x3896c8fc501eBfa40578fE1716081Ec48201c017"
-echo "ZKSYNC Pool address: $ZKSYNC_POOL_ADDRESS"
+ARBITRUM_POOL_ADDRESS=$(echo "$pool_output" | awk '/Deployed to:/ {print $3}')
+# ARBITRUM_POOL_ADDRESS="0x3896c8fc501eBfa40578fE1716081Ec48201c017"
+echo "ARBITRUM Pool address: $ARBITRUM_POOL_ADDRESS"
 
 # # Set the permissions for the pool contract
-echo "Setting the permissions for the pool contract on ZKsync..."
-cast send ${ZKSYNC_REBASE_TOKEN_ADDRESS} "grantMintAndBurnRole(address)" ${ZKSYNC_POOL_ADDRESS} --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount
+echo "Setting the permissions for the pool contract on ARBITRUM..."
+cast send ${ARBITRUM_REBASE_TOKEN_ADDRESS} "grantMintAndBurnRole(address)" ${ARBITRUM_POOL_ADDRESS} --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount
 echo "Pool permissions set"
 
 # # Set the CCIP roles and permissions
-echo "Setting the CCIP roles and permissions on ZKsync..."
-cast send ${ZKSYNC_REGISTRY_MODULE_OWNER_CUSTOM} "registerAdminViaOwner(address)" ${ZKSYNC_REBASE_TOKEN_ADDRESS} --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount
-cast send ${ZKSYNC_TOKEN_ADMIN_REGISTRY} "acceptAdminRole(address)" ${ZKSYNC_REBASE_TOKEN_ADDRESS} --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount
-cast send ${ZKSYNC_TOKEN_ADMIN_REGISTRY} "setPool(address,address)" ${ZKSYNC_REBASE_TOKEN_ADDRESS} ${ZKSYNC_POOL_ADDRESS} --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount
+echo "Setting the CCIP roles and permissions on ARBITRUM..."
+cast send ${ARBITRUM_REGISTRY_MODULE_OWNER_CUSTOM} "registerAdminViaOwner(address)" ${ARBITRUM_REBASE_TOKEN_ADDRESS} --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount
+cast send ${ARBITRUM_TOKEN_ADMIN_REGISTRY} "acceptAdminRole(address)" ${ARBITRUM_REBASE_TOKEN_ADDRESS} --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount
+cast send ${ARBITRUM_TOKEN_ADMIN_REGISTRY} "setPool(address,address)" ${ARBITRUM_REBASE_TOKEN_ADDRESS} ${ARBITRUM_POOL_ADDRESS} --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount
 echo "CCIP roles and permissions set"
 
 # # 2. On Sepolia!
@@ -75,7 +75,7 @@ echo "Configuring the pool on Sepolia..."
 #         bool inboundRateLimiterIsEnabled, false 
 #         uint128 inboundRateLimiterCapacity, 0 
 #         uint128 inboundRateLimiterRate 0 
-forge script ./script/ConfigurePool.s.sol:ConfigurePoolScript --rpc-url ${SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,uint64,address,address,bool,uint128,uint128,bool,uint128,uint128)" ${SEPOLIA_POOL_ADDRESS} ${ZKSYNC_SEPOLIA_CHAIN_SELECTOR} ${ZKSYNC_POOL_ADDRESS} ${ZKSYNC_REBASE_TOKEN_ADDRESS} false 0 0 false 0 0
+forge script ./script/ConfigurePool.s.sol:ConfigurePoolScript --rpc-url ${SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,uint64,address,address,bool,uint128,uint128,bool,uint128,uint128)" ${SEPOLIA_POOL_ADDRESS} ${ARBITRUM_SEPOLIA_CHAIN_SELECTOR} ${ARBITRUM_POOL_ADDRESS} ${ARBITRUM_REBASE_TOKEN_ADDRESS} false 0 0 false 0 0
 
 # Deposit funds to the vault
 echo "Depositing funds to the vault on Sepolia..."
@@ -83,16 +83,16 @@ cast send ${VAULT_ADDRESS} --value ${AMOUNT} --rpc-url ${SEPOLIA_RPC_URL} --acco
 
 # Wait a beat for some interest to accrue
 
-# Configure the pool on ZKsync
-echo "Configuring the pool on ZKsync..."
-# cast send ${ZKSYNC_POOL_ADDRESS}  --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount "applyChainUpdates(uint64[],(uint64,bytes[],bytes,(bool,uint128,uint128),(bool,uint128,uint128))[])" "[${SEPOLIA_CHAIN_SELECTOR}]" "[(${SEPOLIA_CHAIN_SELECTOR},[$(cast abi-encode "f(address)" ${SEPOLIA_POOL_ADDRESS})],$(cast abi-encode "f(address)" ${SEPOLIA_REBASE_TOKEN_ADDRESS}),(false,0,0),(false,0,0))]"
-forge script ./script/ConfigurePool.s.sol:ConfigurePoolScript --rpc-url ${ZKSYNC_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,uint64,address,address,bool,uint128,uint128,bool,uint128,uint128)" ${ZKSYNC_POOL_ADDRESS} ${SEPOLIA_CHAIN_SELECTOR} ${SEPOLIA_POOL_ADDRESS} ${SEPOLIA_REBASE_TOKEN_ADDRESS} false 0 0 false 0 0
+# Configure the pool on ARBITRUM
+echo "Configuring the pool on ARBITRUM..."
+# cast send ${ARBITRUM_POOL_ADDRESS}  --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount "applyChainUpdates(uint64[],(uint64,bytes[],bytes,(bool,uint128,uint128),(bool,uint128,uint128))[])" "[${SEPOLIA_CHAIN_SELECTOR}]" "[(${SEPOLIA_CHAIN_SELECTOR},[$(cast abi-encode "f(address)" ${SEPOLIA_POOL_ADDRESS})],$(cast abi-encode "f(address)" ${SEPOLIA_REBASE_TOKEN_ADDRESS}),(false,0,0),(false,0,0))]"
+forge script ./script/ConfigurePool.s.sol:ConfigurePoolScript --rpc-url ${ARBITRUM_SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,uint64,address,address,bool,uint128,uint128,bool,uint128,uint128)" ${ARBITRUM_POOL_ADDRESS} ${SEPOLIA_CHAIN_SELECTOR} ${SEPOLIA_POOL_ADDRESS} ${SEPOLIA_REBASE_TOKEN_ADDRESS} false 0 0 false 0 0
 
-# Bridge the funds using the script to zksync 
-echo "Bridging the funds using the script to ZKsync..."
+# Bridge the funds using the script to ARBITRUM 
+echo "Bridging the funds using the script to ARBITRUM..."
 SEPOLIA_BALANCE_BEFORE=$(cast balance $(cast wallet address --account myaccount) --erc20 ${SEPOLIA_REBASE_TOKEN_ADDRESS} --rpc-url ${SEPOLIA_RPC_URL})
 echo "Sepolia balance before bridging: $SEPOLIA_BALANCE_BEFORE"
-forge script ./script/BridgeTokens.s.sol:BridgeTokensScript --rpc-url ${SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,address,uint64,address,uint256,address)" $(cast wallet address --account myaccount) ${SEPOLIA_LINK_ADDRESS} ${ZKSYNC_SEPOLIA_CHAIN_SELECTOR} ${SEPOLIA_REBASE_TOKEN_ADDRESS} ${AMOUNT} ${SEPOLIA_ROUTER}
-echo "Funds bridged to ZKsync"
+forge script ./script/BridgeTokens.s.sol:BridgeTokensScript --rpc-url ${SEPOLIA_RPC_URL} --account myaccount --broadcast --verify --gas-price 250000000  --sig "run(address,address,uint64,address,uint256,address)" $(cast wallet address --account myaccount) ${SEPOLIA_LINK_ADDRESS} ${ARBITRUM_SEPOLIA_CHAIN_SELECTOR} ${SEPOLIA_REBASE_TOKEN_ADDRESS} ${AMOUNT} ${SEPOLIA_ROUTER}
+echo "Funds bridged to ARBITRUM"
 SEPOLIA_BALANCE_AFTER=$(cast balance $(cast wallet address --account myaccount) --erc20 ${SEPOLIA_REBASE_TOKEN_ADDRESS} --rpc-url ${SEPOLIA_RPC_URL})
 echo "Sepolia balance after bridging: $SEPOLIA_BALANCE_AFTER"
